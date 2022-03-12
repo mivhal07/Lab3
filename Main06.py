07cec73e26c4cf9ae01602372da3513cb3e1785b
mivhal07 <michaldob07@gmail.com>
2022-03-12 19:22:53 +0100

    cwiczenia 12.03

--- Main06.py
+++ Main06.py
@@ -1,6 +1,14 @@
-def ciąg(a):
-    return a * 2
+def ciąg(a1, b, ile):
+    ilosc = a1 + ile;
+    # a10 = 1 + 9 * 4
+    # a9 = 1 + 8 * 4
+    # a3 = 1 + 2 * 4
+    # a2 = 1 + 1 * 4
+    # a1 = 1
+    result = 0
+    for x in range(a1, ilosc):
+        result += x + (x - 1) * b
+        return result
 
 
-liczba = input("Ktory element ciąglu chcesz policzyć? Napisz liczbę: ")
-print(ciąg(int(liczba)))
+print(ciąg(1, 4, 10))
